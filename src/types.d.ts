b4952ffe71f39146fa3770814f7b733d4a4b029a
juan pablo <socito2000@gmail.com>
2025-08-04 14:50:54 -0400

    MIGRATE TO CMS2: Use cms2.json with fecha field - Complete migration to structured dates

--- src/types.d.ts
+++ src/types.d.ts
@@ -12,6 +12,24 @@ declare module '../data/cms1.json' {
   export default noticias;
 }
 
+declare module '../data/cms2.json' {
+  interface NoticiaCMS2 {
+    fecha: string;
+    texto: string;
+    imagen: string;
+    link: string;
+  }
+  interface CMS2Data {
+    sitio_web: string;
+    url: string;
+    fecha_scraping: string;
+    total_noticias: number;
+    noticias: NoticiaCMS2[];
+  }
+  const cms2Data: CMS2Data;
+  export default cms2Data;
+}
+
 declare module '../../data/cms1.json' {
   interface Noticia {
     text: string;
